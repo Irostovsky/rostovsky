2da51a05f42898a9c87f66132224bf5edb21f1de
Ivan Rostovsky <ir@anahoret.com>
2010-09-28 19:10:11 +0300

    Added admin/home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :sites
   map.resources :locales, :only => :update
+  
+  map.namespace :admin do |admin|
+    admin.resources :home, :only => :index
+  end
 
   map.root :controller => "sites"
+
+  map.with_options :controller => "admin/home", :action => "index" do |f|
+    f.admin "admin"
+  end
 
 end
