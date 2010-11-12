e209f275823420679b1653b27e23670fa4ab41f0
Ivan Rostovsky <ir@anahoret.com>
2010-11-12 19:08:58 +0200

    Editing photos

--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -8,14 +8,28 @@ class Admin::PhotosController < ApplicationController
     @photo = Photo.new
   end
   
+  def edit
+    @photo = @site.photos.find(params[:id])
+  end
+  
   def create
     @photo = Photo.new(params[:photo])
     if @photo.valid? && @site.photos << @photo
-      redirect_to admin_site_photos_path(@site), notice => 'Photo was successfully created.'
+      redirect_to admin_site_photos_path(@site), :notice => 'Photo was successfully created.'
     else
       render :action => :index
     end
   end
+
+  def update
+    @photo = @site.photos.find(params[:id])
+    if @photo.update_attributes(params[:photo])
+     redirect_to admin_site_photos_path(@site), :notice => 'Photo was successfully updated.'
+    else
+      render :action => :edit
+    end
+  end
+
   private
   
   def find_site
